b1b420787309879cbc12f80d9544e563d294e6ff
mleano <mleano@mail.greenriver.edu>
2017-10-21 22:13:43 -0700

    Added validation function that checks if an input is entered. Made sure an input is made before moving onto the analysis view.

--- StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/InputActivity.java
+++ StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/InputActivity.java
@@ -1,11 +1,13 @@
 package co.miniforge.corey.stringanalyzer;
 
 import android.content.Intent;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class InputActivity extends AppCompatActivity {
     public static String intentTag = "inputData";
@@ -32,10 +34,28 @@ public class InputActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), AnalyzerActivity.class);
-                intent.putExtra(intentTag, input.getText().toString());
-                startActivity(intent);
+                //Make sure an input is entered
+                if (validateInput(input.getText().toString()) == false ) {
+                    Toast.makeText(getApplicationContext(), R.string.error_no_msg, Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), AnalyzerActivity.class);
+                    intent.putExtra(intentTag, input.getText().toString());
+                    startActivity(intent);
+                }
+
+
             }
         });
+    }
+
+    //Function to make sure an input is entered
+    private boolean validateInput(String input) {
+        boolean isValid = true;
+
+        if (input.length() < 1) {
+            isValid = false;
+        }
+
+        return isValid;
     }
 }
